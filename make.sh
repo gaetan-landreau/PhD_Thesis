78d073642851fe7495a3c83c1ad986ba1bd78ff4
gaetan-landreau <gaetan.landreau@meero.com>
2024-04-12 16:37:53 +0000

    updt 12/05

--- make.sh
+++ make.sh
@@ -1,9 +1,9 @@
-rm -rf main.aux main.tex.bbl main.bcf main.maf main.fdb_latexmk main.fls main.idx main.lof main.log main.lot main.mtc main.mtc* main.out main.pdf main.run.xml main.synctex\(busy\) main.synctex.gz main.toc
+rm -rf main.aux main.tex.bbl main.bcf main.maf main.fdb_latexmk main.fls main.idx main.lof main.log main.lot main.mtc main.mtc* main.out main.run.xml main.synctex\(busy\) main.synctex.gz main.toc
 rm -rf indent.log
 
 pdflatex -halt-on-error main.tex
 biber main
 pdflatex -halt-on-error main.tex
-pdflatex -halt-on-error main.tex
+#pdflatex -halt-on-error main.tex
 rm -rf main.aux main.bcf main.tex.bbl main.maf main.fdb_latexmk main.fls main.idx main.lof main.log main.lot main.mtc main.mtc* main.out main.run.xml main.synctex\(busy\) main.synctex.gz main.toc
 rm -rf indent.log
